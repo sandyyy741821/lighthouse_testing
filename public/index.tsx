5e923e3e885d99aebfdba3c589a2f2ec093a0044
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-06-03 22:16:04 +0530

    yml

---
+++ public/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:400,700"
+        />
+        <title>MyReads</title>
+      </header>
+      <main>
+        <h1>Welcome to MyReads</h1>
+        <p>This is your app running in React with TypeScript.</p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
